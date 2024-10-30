f916cae66a97c22e9f390a3c64cf997aa023c79c
Siva Lingala <sl196@sciits.com>
2024-10-30 12:07:53 +0530

    Fourth commit

--- src/test/java/testCases/TC012_InvestigateMBC.java
+++ src/test/java/testCases/TC012_InvestigateMBC.java
@@ -1,45 +1,64 @@
 package testCases;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import pageObjects.InvestigateMBC;
 import pageObjects.LoginPage;
 import pageObjects.RegisterMBC;
 
-public class TC012_InvestigateMBC extends BaseClass{
-	
-	public InvestigateMBC investigate;
+public class TC012_InvestigateMBC extends BaseClass {
 
+	public InvestigateMBC investigate;
+@Ignore
 	@Test
 	public void Verify_TransferShares() {
-		
 
-		
-	LoginPage loginpage = new LoginPage(driver);
+		try {
 
-	loginpage.SetUserName("rocasuperuser");
-	loginpage.SetPassword("Welcome_1!");
-	loginpage.SetSingin();
-	
-	RegisterMBC regMBC = new RegisterMBC(driver);
+			LoginPage loginpage = new LoginPage(driver);
 
-	regMBC.MenuButton();
-	
-     investigate=new InvestigateMBC(driver);
-     
-     investigate.SetCourtOrder();
-     
-     investigate.SetInvestigate();
-     
-     investigate.SetMBCNumber("MBC606784926IND");
-     
-     investigate.SearchButton();
-     
-     investigate.SetCalSelect();
-     
-     investigate.SetDefraud();
+			loginpage.SetUserName("rocasuperuser");
+			loginpage.SetPassword("Welcome_1!");
+			loginpage.SetSingin();
 
-	
+			RegisterMBC regMBC = new RegisterMBC(driver);
 
-}
+			regMBC.MenuButton();
+
+			investigate = new InvestigateMBC(driver);
+
+			investigate.SetCourtOrder();
+
+			investigate.SetInvestigate();
+
+			investigate.SetMBCNumber("MBC606784926IND");
+
+			investigate.SearchButton();
+
+			investigate.SetCalSelect();
+
+			investigate.SetDefraud();
+
+			investigate.SetFirstName("Tom");
+
+			investigate.SetLastName("Latham");
+
+			investigate.SetSumbit();
+
+			investigate.SetInvestigate();
+
+			String msg = investigate.GetAlertMgs();
+
+			assertEquals(msg, "Application to the court for Investigation is Successful");
+		} catch (Exception e) {
+
+			Assert.fail();
+		}
+	}
+
 }
